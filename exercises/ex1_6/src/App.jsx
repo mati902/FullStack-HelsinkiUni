880b6a9c3d70923e85bf03c49062cbdb8952a113
Mateusz Kupis <kupismateusz3993@gmail.com>
2023-10-26 14:21:19 +0200

    some changes

--- exercises/ex1_6/src/App.jsx
+++ exercises/ex1_6/src/App.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
+
+import React, { useState } from 'react';
+
+const Statistics = (props) => {
+  good + " " + setGood
+  neutral  + " " + setNeutral
+  bad + " " + setBad
+}
+
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text} </button>
+  )
 
+const setToGood = NewGood => {
+  console.log("value of good: ", NewGood)
+  setGood(NewGood)
+}
+const setToNeutral = NewNeutral => {
+  console.log("value of good: ", NewNeutral)
+  setNeutral(NewNeutral)
+}
+const setToBad = NewBad => {
+  console.log("value of good: ", NewBad)
+  setGood(NewBad)
+}
+  
   return (
     <div>
       <h2>give feedback</h2>
-      
+       
+      <Button handleClick={() => setToGood(good + 1)} text="good"/>
+  <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral"/>
+  <Button handleClick={() => setToBad(bad + 1)} text="bad"/>
+
+   
+
     </div>
   )
 }
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,55 +1,56 @@
 
 
-function App() {
-  const History = (props) => {
-    if (props.allClicks.length === 0) {
+  const App = () => {
+    const History = (props) => {
+      if (props.allClicks.length === 0) {
+        return (
+          <div>
+            the app is used by pressing the buttons
+          </div>
+        )
+      }
+    
       return (
         <div>
-          the app is used by pressing the buttons
+          button press history: {props.allClicks.join(' ')}
         </div>
       )
     }
-  
-    return (
-      <div>
-        button press history: {props.allClicks.join(' ')}
-      </div>
+    
+    
+    const Button = ({ handleClick, text }) => (
+      <button onClick={handleClick}>
+        {text}
+      </button>
     )
-  }
-  
-  
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-  
-  const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-  
-    const handleLeftClick = () => {
-      setAll(allClicks.concat('L'))
-      setLeft(left + 1)
+    
+    const App = () => {
+      const [left, setLeft] = useState(0)
+      const [right, setRight] = useState(0)
+      const [allClicks, setAll] = useState([])
+    
+      const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+      }
+    
+      const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+      }
+    
+      return (
+        <div>
+          {left}
+    
+          <Button handleClick={handleLeftClick} text='left' />
+          <Button handleClick={handleRightClick} text='right' />
+          {right}
+          <History allClicks={allClicks} />
+        </div>
+      )
     }
-  
-    const handleRightClick = () => {
-      setAll(allClicks.concat('R'))
-      setRight(right + 1)
-    }
-  
-    return (
-      <div>
-        {left}
-  
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
-    )
-  }
-}
 
+    
+  }
 export default App
