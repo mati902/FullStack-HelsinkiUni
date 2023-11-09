868924b9574c94c66392e4cd0718de86c09a2e7b
Mateusz Kupis <kupismateusz3993@gmail.com>
2023-11-09 14:24:44 +0100

    new changes to ex1_6

--- exercises/ex1_6/src/App.jsx
+++ exercises/ex1_6/src/App.jsx
@@ -5,6 +5,7 @@ const Statistics = (props) => {
   good + " " + setGood
   neutral  + " " + setNeutral
   bad + " " + setBad
+  all + " " + setAll
 }
 
 
@@ -19,6 +20,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   const Button = (props) => (
     <button onClick={props.handleClick}>{props.text} </button>
   )
@@ -28,23 +30,33 @@ const setToGood = NewGood => {
   setGood(NewGood)
 }
 const setToNeutral = NewNeutral => {
-  console.log("value of good: ", NewNeutral)
+  console.log("value of neutral: ", NewNeutral)
   setNeutral(NewNeutral)
 }
 const setToBad = NewBad => {
-  console.log("value of good: ", NewBad)
-  setGood(NewBad)
+  console.log("value of bad: ", NewBad)
+  setBad(NewBad)
 }
-  
+const setToAll = NewAll => {
+  console.log("value of all: ", NewGood + NewNeutral + NewBad)
+  setAll(NewAll)
+}
   return (
     <div>
       <h2>give feedback</h2>
        
-      <Button handleClick={() => setToGood(good + 1)} text="good"/>
-  <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral"/>
-  <Button handleClick={() => setToBad(bad + 1)} text="bad"/>
+  <Button  handleClick={() => setToGood(good + 1)} text="good"/>
+  <Button   handleClick={() => setToNeutral(neutral + 1)} text="neutral"/>
+  <Button   handleClick={() => setToBad(bad + 1)}   text="bad"/>
 
    
+
+<h2>statistics</h2>
+
+good {good}<br/>
+neutral  {neutral}<br/>
+bad  {bad}<br/>
+all {all}
 
     </div>
   )
